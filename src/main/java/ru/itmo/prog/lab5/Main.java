8e7ccf72c11bab8896821f7ecdc7c1a2744b6582
zevtos <vladik.pahomov@mail.ru>
2024-02-17 13:46:39 +0300

    1.1.14

--- src/main/java/ru/itmo/prog/lab5/Main.java
+++ src/main/java/ru/itmo/prog/lab5/Main.java
@@ -28,12 +28,12 @@ public class Main {
         var console = new StandardConsole();
 
         Signal.handle(new Signal("INT"),  // SIGINT
-                signal -> System.out.println("Для получения справки введите 'help', для завершения программы введите 'exit" + '\n' + console.getPrompt()));
+                signal -> System.out.print("Для получения справки введите 'help', для завершения программы введите 'exit" + '\n' + console.getPrompt()));
         Signal.handle(new Signal("TERM"),  // SIGINT
-                signal -> System.out.println("Для получения справки введите 'help', для завершения программы введите 'exit" + '\n' + console.getPrompt()));
+                signal -> System.out.print("Для получения справки введите 'help', для завершения программы введите 'exit" + '\n' + console.getPrompt()));
         try {
             Signal.handle(new Signal("TSTP"),  // SIGINT
-                    signal -> System.out.println("Для получения справки введите 'help', для завершения программы введите 'exit" + '\n' + console.getPrompt()));
+                    signal -> System.out.print("Для получения справки введите 'help', для завершения программы введите 'exit" + '\n' + console.getPrompt()));
         }catch (IllegalArgumentException ignored){}
 
         checkFileArgument(args, console);
